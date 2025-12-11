34f68a98c89ed5352f784abd7a665588c8451331
Phạm Quốc Khải <164444822+BuuCoder@users.noreply.github.com>
2025-12-11 18:32:34 +0700

    Update PostLayout.tsx

--- src/app/tin-tuc/[slug]/PostLayout.tsx
+++ src/app/tin-tuc/[slug]/PostLayout.tsx
@@ -1,5 +1,7 @@
 // app/posts/PostLayout.tsx
-import { ReactNode } from "react";
+"use client";
+
+import { ReactNode, useEffect } from "react";
 import { Breadcrumb } from "./component/Breadcrumb";
 import { Sidebar } from "./component/Sidebar";
 import { Article } from "./component/Article";
@@ -16,6 +18,11 @@ export default function PostLayout({
   relatedPosts,
   children,
 }: PostLayoutProps) {
+  // Scroll to top khi vào trang
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-50 to-white">
       <Breadcrumb title={post.title} />
